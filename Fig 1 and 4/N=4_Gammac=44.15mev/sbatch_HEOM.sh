7b76659692df8051cea96b3a0a53e2693690a014
Wenxiang Ying <82459822+Okita0512@users.noreply.github.com>
2024-07-02 15:04:15 -0400

    Add files via upload

---
+++ Fig 1 and 4/N=4_Gammac=44.15mev/sbatch_HEOM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p debug 
+#SBATCH -o output.log
+#SBATCH --mem=8GB
+#SBATCH --time=0-01:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=HEOM
+#SBATCH --open-mode=append
+
+time ../../bin/1d-resp ./input.json
+
